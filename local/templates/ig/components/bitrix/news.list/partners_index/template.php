f464fd5dfc737a78ec6b190d050aa7a2322e4904
Oleg Makeev <wotmc@yandex.ru>
2019-09-07 18:39:51 +0300

    component external links <!--noindex-->, code cleanup

--- local/templates/ig/components/bitrix/news.list/partners_index/template.php
+++ local/templates/ig/components/bitrix/news.list/partners_index/template.php
@@ -1,4 +1,7 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<?php
+if (!defined('B_PROLOG_INCLUDED') || B_PROLOG_INCLUDED !== true) {
+    die();
+}
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -13,41 +16,53 @@
 $this->setFrameMode(true);
 ?>
 <div class="section section--block" id="partners" data-goto-offset-element=".header">
-	<div class="container">
-		<h2 class="h1 text-align-center"><?=$arResult["NAME"]?></h2>
-		<div class="p text text-align-center mw940 margin-auto">
-			<p><?$APPLICATION->IncludeComponent(
-					"bitrix:main.include",
-					"",
-					Array(
-						"AREA_FILE_SHOW" => "file",
-						"AREA_FILE_SUFFIX" => "inc",
-						"EDIT_TEMPLATE" => "",
-						"PATH" => "/local/inc_area/partners-text.php"
-					)
-				);?><br>
-				<a class="link--ib link--bordered" href="<?=$arResult["LIST_PAGE_URL"]?>">Все партнеры</a></p>
-		</div><?
-		if(!empty($arResult["ITEMS"])) {?>
-		<div class="partners">
-			<div class="partners__inner"><?
-				foreach($arResult["ITEMS"] as $arItem) {?>
-					<div class="partner">
-					<div class="partner__inner"><?
-						if($arItem["PREVIEW_PICTURE"]) {?>
-							<div class="partner__image">
-								<img data-lazy-src="<?=$arItem["PREVIEW_PICTURE"]["SRC"]?>" alt="">
-							</div><?
-						}?>
-						<div class="partner__title"><?=$arItem["NAME"]?></div><?
-						if($arItem["PROPERTIES"]["LINK"]["VALUE"]) {?>
-							<div class="partner__summary"><a class="link--ib link--bordered text-ellipsis" target="_blank" href="<?=$arItem["PROPERTIES"]["LINK"]["VALUE"]?>"><?=str_replace(array("http://", "https://"), '', $arItem["PROPERTIES"]["LINK"]["VALUE"])?></a></div><?
-						}?>
-					</div>
-					</div><?
-				}?>
-			</div>
-		</div><?
-		}?>
-	</div>
+    <div class="container">
+        <h2 class="h1 text-align-center"><?= $arResult['NAME'] ?></h2>
+        <div class="p text text-align-center mw940 margin-auto">
+            <p>
+                <?php $APPLICATION->IncludeComponent(
+                    'bitrix:main.include',
+                    '',
+                    Array(
+                        'AREA_FILE_SHOW' => 'file',
+                        'AREA_FILE_SUFFIX' => 'inc',
+                        'EDIT_TEMPLATE' => '',
+                        'PATH' => '/local/inc_area/partners-text.php'
+                    )
+                ); ?>
+                <br>
+                <a class="link--ib link--bordered" href="<?= $arResult['LIST_PAGE_URL'] ?>">Все партнеры</a>
+            </p>
+        </div>
+        <?php if ($arResult['ITEMS'] !== []): ?>
+            <div class="partners">
+                <div class="partners__inner">
+                    <?php foreach ($arResult['ITEMS'] as $arItem) : ?>
+                        <div class="partner">
+                            <div class="partner__inner">
+                                <?php if ($arItem['PREVIEW_PICTURE']) : ?>
+                                    <div class="partner__image">
+                                        <img data-lazy-src="<?= $arItem['PREVIEW_PICTURE']['SRC'] ?>" alt="" src="">
+                                    </div>
+                                <?php endif; ?>
+                                <div class="partner__title">
+                                    <?= $arItem['NAME'] ?>
+                                </div>
+                                <?php if ($arItem['PROPERTIES']['LINK']['VALUE']) : ?>
+                                    <div class="partner__summary">
+                                        <!--noindex-->
+                                        <a rel="nofollow" class="link--ib link--bordered text-ellipsis" target="_blank"
+                                           href="<?= $arItem['PROPERTIES']['LINK']['VALUE'] ?>">
+                                            <?= str_replace(['http://', 'https://'], '', $arItem['PROPERTIES']['LINK']['VALUE']) ?>
+                                        </a>
+                                        <!--/noindex-->
+                                    </div>
+                                <?php endif; ?>
+                            </div>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+            </div>
+        <?php endif; ?>
+    </div>
 </div>
