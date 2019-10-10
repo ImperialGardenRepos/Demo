a00d141bd52c85400a159a519a1304da94b31327
Oleg Makeev <wotmc@yandex.ru>
2019-10-11 01:08:50 +0300

    XML sitemap generator

--- local/php_interface/constants.php
+++ local/php_interface/constants.php
@@ -1,10 +1,21 @@
 <?php
+defined('SITE_BASE_DIR') or define('SITE_BASE_DIR', '/var/www/stage.imperialgarden.ru/');
 defined('SITE_TEMPLATE_PATH') or define('SITE_TEMPLATE_PATH', '/local/templates/ig');
 
-defined('CONSTRUCTOR_IBLOCK_ID') or define('CONSTRUCTOR_IBLOCK_ID', 34);
 defined('SERVICE_IBLOCK_TYPE') or define('SERVICE_IBLOCK_TYPE', 'services');
+defined('CATALOG_IBLOCK_TYPE') or define('CATALOG_IBLOCK_TYPE', 'catalog');
 
 defined('CATALOG_IBLOCK_ID') or define('CATALOG_IBLOCK_ID', 1);
 defined('CATALOG_SKU_IBLOCK_ID') or define('CATALOG_SKU_IBLOCK_ID', 2);
+defined('NEWS_IBLOCK_ID') or define('NEWS_IBLOCK_ID', 5);
+defined('SERVICES_IBLOCK_ID') or define('SERVICES_IBLOCK_ID',6);
+defined('FAQ_IBLOCK_ID') or define('FAQ_IBLOCK_ID',14);
 defined('CATALOG_GARDEN_IBLOCK_ID') or define('CATALOG_GARDEN_IBLOCK_ID', 17);
-defined('CATALOG_IBLOCK_TYPE') or define('CATALOG_IBLOCK_TYPE', 'catalog');
+defined('ENTERTAINMENT_IBLOCK_ID') or define('ENTERTAINMENT_IBLOCK_ID', 20);
+defined('TEAM_IBLOCK_ID') or define('TEAM_IBLOCK_ID', 21);
+defined('GRATITUDE_IBLOCK_ID') or define('GRATITUDE_IBLOCK_ID', 22);
+defined('REVIEWS_IBLOCK_ID') or define('REVIEWS_IBLOCK_ID', 23);
+defined('PARTNERS_IBLOCK_ID') or define('PARTNERS_IBLOCK_ID', 24);
+defined('PORTFOLIO_IBLOCK_ID') or define('PORTFOLIO_IBLOCK_ID', 25);
+defined('VEHICLE_RENT_IBLOCK_ID') or define('VEHICLE_RENT_IBLOCK_ID',33);
+defined('CONSTRUCTOR_IBLOCK_ID') or define('CONSTRUCTOR_IBLOCK_ID', 34);
