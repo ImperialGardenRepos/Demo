7322a5426b370713bc2f02010f691ec1f032c818
Oleg Makeev <wotmc@yandex.ru>
2019-12-02 08:51:10 +0300

    projects

--- promo/index.php
+++ promo/index.php
@@ -1,44 +1,18 @@
 <?php
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
-$APPLICATION->SetTitle("Спецпредложения");
-?><div class="section section--grey section--article section--fullheight text">
-    <div class="container">
-        <div class="cols-wrapper cols-wrapper--h1-stabs">
-            <div class="cols">
-                <div class="col col--heading">
-                    <h1>Спецпредложения</h1>
-                </div>
-            </div>
-        </div>
-        <div class="tgbs tgbs--mm">
-            <div class="tgbs__inner">
-                <div class="tgb tgb--1-2 tgb--h50 tgb--img-hover">
-                    <a class="tgb__inner" href="/promo/bolshie-derevya/">
-                        <div class="tgb__image img-to-bg img-to-bg--inited" style="/upload/iblock/09c/bolshie-derevya_baner_3-_1_1200.jpg">
-                            <img alt="Большие деревья" src="/upload/iblock/13d/bolshie_derevya_baner_3_1_1200.jpg" data-lazy-src="tgb__image">
-                        </div>
-                        <div class="tgb__overlay">
-                            <div class="tgb__overlay-part">
-                                <div class="tgb__overlay-part-inner">
-                                    <div class="tgb__title h3">
-                                        Большие деревья
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="tgb__overlay-part mobile-hide">
-                                <div class="tgb__overlay-part-inner">
-                                    <div class="text">
-                                        <p>
-                                            Больше не нужно ждать, когда маленький саженец превратится во взрослое, большое дерево.
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<?php require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/header.php';
+$APPLICATION->SetTitle('Спецпредложения');
+
+$APPLICATION->IncludeComponent(
+    'bitrix:catalog.section.list',
+    'landings',
+    [
+        'IBLOCK_TYPE' => SERVICE_IBLOCK_TYPE,
+        'IBLOCK_ID' => CONSTRUCTOR_IBLOCK_ID,
+        'SECTION_CODE' => 'promo',
+        'SEF_FOLDER' => '/promo/',
+        'ADD_SECTIONS_CHAIN' => 'N'
+    ],
+    false
+);
+
+require $_SERVER['DOCUMENT_ROOT'] . '/bitrix/footer.php';
